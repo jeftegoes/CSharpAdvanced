179e65aaddf870a0acf4379446a4e4cf783627fa
jeftegoes <jefte.goes.dev@outlook.com>
2022-07-16 23:51:47 -0300

    Update NullableTypes project to .NET 6 and update README.md file.

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -1,45 +1,50 @@
-﻿using System;
+﻿/* 
+    - This expression don't be possible, because DateTime is a value type, and value type cannot be null.
+    DateTime date = null;
+*/
 
-namespace NullableTypes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            /*
-                Nullable<DateTime> date = null;
 
-                DateTime? date = null;
-            
-                int? number = null;
+/*
+    - Nullable is a generic structure defined in the System namespace.
+    Nullable<DateTime> date = null;
 
-                Console.WriteLine("GetValueOrDefault(): Date: {0} Number: {1}", date.GetValueOrDefault(), number.GetValueOrDefault());
-                Console.WriteLine("HasValue: Date: {0} Number: {1}", date.HasValue, number.HasValue);
-                
-                System.InvalidOperationException: Nullable object must have a value.
-                Console.WriteLine("Value: {0} Number: {1}", date.Value, number.Value);
-            */
-            
-            /*
-                DateTime? date = new DateTime(2014, 1, 1);
-                DateTime date2 = date.GetValueOrDefault();
-                DateTime? date3 = date2;
+    - Short expression.
+    DateTime? date = null;
 
-                Console.WriteLine("GetValueOrDefault(): Date: {0}", date3.GetValueOrDefault());
-            */
-            DateTime? date = null;
-            // Short form...
-            DateTime date2 = date ?? DateTime.Today;
-            DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+    int? number = null;
 
-            /* Long form...
-                if (date != null)
-                    date2 = date.GetValueOrDefault();
-                else
-                    date2 = DateTime.Today;
-            */
+    Console.WriteLine("GetValueOrDefault(): Date: {0} Number: {1}", date.GetValueOrDefault(), number.GetValueOrDefault());
+    Console.WriteLine("HasValue: Date: {0} Number: {1}", date.HasValue, number.HasValue);
+    
+    - In the expression below, this error will be returned: System.InvalidOperationException: Nullable object must have a value.
+    - Because date.Value and number.Value don't has value
+    Console.WriteLine("Value: {0} Number: {1}", date.Value, number.Value);
+*/
 
-            Console.WriteLine("Date: {0}", date2);
-        }
-    }
-}
+
+/*
+    DateTime? date = new DateTime(1991, 04, 10);
+    DateTime date2 = date.GetValueOrDefault();
+    DateTime? date3 = date2;
+
+    Console.WriteLine("GetValueOrDefault(): Date: {0}", date3.GetValueOrDefault());
+*/
+
+
+/*
+    - This example demonstrate how to use the Null Coalescence Operator.
+    - Here don't use operator, is a long expression.
+    DateTime? date = null;
+    DateTime date2;
+
+    if (date != null)
+        date2 = date.GetValueOrDefault();
+    else
+        date2 = DateTime.Today;
+*/
+
+// This expression using Null Coalescence Operator ??
+DateTime? date = null;
+DateTime date2 = date ?? DateTime.Today;
+DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+Console.WriteLine("Date: {0}", date2);
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - [1. Delagates](#1-delagates)
 - [2. Events and Delegates](#2-events-and-delegates)
+- [Nullable types](#nullable-types)
 
 ## 1. Delagates
 
@@ -15,9 +16,18 @@
 - Interfaces or Delegates? Use a delegate when:
   - An eventing design pattern is used.
   - The caller doesn't need to access other properties or methods on the object implementing the method.
+- [Delegate example](Delegates/)
 
 ## 2. Events and Delegates
 
 - A mechanism for communication between objects.
 - Used in building _Loosely Coupled Application_.
 - Helps extending application.
+- [Events and Delegates example](EventsAndDelegates/)
+
+## Nullable types
+
+- This expression `DateTime date = null;` don't be possible, because DateTime is a value type, and value type cannot be null.
+- `Nullable<DateTime> date = null;` is a generic structure defined in the System namespace.
+- The null-coalescing operator ?? returns the value of its left-hand operand if it isn't null; otherwise, it evaluates the right-hand operand and returns its result. The ?? operator doesn't evaluate its right-hand operand if the left-hand operand evaluates to non-null.
+- [Nullable types example](NullableTypes/)
